309c467d49a3f6ad81a08a86dca484c412ab3508
Suman <borosuman@gmail.com>
2022-04-05 15:05:25 +0530

    Permission features only

--- app/src/main/java/in/nic/assam/libraries/capturegeotaggedimage/CameraControlActivity.java
+++ app/src/main/java/in/nic/assam/libraries/capturegeotaggedimage/CameraControlActivity.java
@@ -3,40 +3,25 @@ package in.nic.assam.libraries.capturegeotaggedimage;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.location.Location;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Objects;
 
-import in.nic.assam.libraries.capturegeotaggedimage.image.ImageMetaData;
-import in.nic.assam.libraries.capturegeotaggedimage.utils.LocationUtils;
 import in.nic.assam.libraries.capturegeotaggedimage.utils.PermissionUtils;
 
-public class CameraControlActivity extends AppCompatActivity implements PermissionUtils.PermissionResultCallback, LocationUtils.LocationResultCallback {
+public class CameraControlActivity extends AppCompatActivity implements PermissionUtils.PermissionResultCallback {
 
     public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
     private static final String TAG = CameraControlActivity.class.getSimpleName();
-    public final String APP_TAG = "CaptureGeoTaggedImage";
-    public String photoFileName = "photo.jpg";
-    File photoFile;
     private final ArrayList<String> permissionsList = new ArrayList<>();
     private PermissionUtils permissionUtils;
     private boolean isPermissionGranted;
-    private Location location;
-    private double latitude;
-    private double longitude;
-    private LocationUtils locationUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,20 +43,21 @@ public class CameraControlActivity extends AppCompatActivity implements Permissi
     public void PermissionGranted(int request_code) {
         Log.i("PERMISSION", "GRANTED");
         isPermissionGranted = true;
-        Toast.makeText(CameraControlActivity.this, "Permisssion granted ", Toast.LENGTH_SHORT).show();
-
-        locationUtils = new LocationUtils(CameraControlActivity.this);
-        locationUtils.getLocation();
+        Toast.makeText(CameraControlActivity.this, "Permission granted ", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void PartialPermissionGranted(int request_code, ArrayList<String> granted_permissions) {
         Log.i("PERMISSION PARTIALLY", "GRANTED");
+        Toast.makeText(CameraControlActivity.this, "Permission partially granted ", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
     public void PermissionDenied(int request_code) {
         Log.i("PERMISSION", "DENIED");
+        Toast.makeText(CameraControlActivity.this, "Permission denied ", Toast.LENGTH_SHORT).show();
+
         finish();
     }
 
@@ -96,8 +82,7 @@ public class CameraControlActivity extends AppCompatActivity implements Permissi
 
             if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
                 if (resultCode == RESULT_OK) {
-                    ImageMetaData image = new ImageMetaData(photoFile.getAbsolutePath(),location);
-                    sendDataToCallerActivity(image);
+                    sendDataToCallerActivity();
                     Toast.makeText(this, "Picture was taken!", Toast.LENGTH_SHORT).show();
 
                 } else { // Result was a failure
@@ -107,61 +92,13 @@ public class CameraControlActivity extends AppCompatActivity implements Permissi
         }
     }
 
-    private void sendDataToCallerActivity(ImageMetaData image) {
+    private void sendDataToCallerActivity() {
         Intent resultIntent = new Intent();
-        resultIntent.putExtra("captured_image",image);
+        resultIntent.putExtra("data","test");
         setResult(Activity.RESULT_OK, resultIntent);
         finish();
     }
 
-    @Override
-    public void onLocationResult(Location location) {
-        this.location = location;
-        latitude = location.getLatitude();
-        longitude = location.getLongitude();
-        Toast.makeText(CameraControlActivity.this, "Location retrieved: " + latitude + " " + longitude, Toast.LENGTH_SHORT).show();
-        if (isPermissionGranted) {
-            launchCamera();
-        }
-    }
 
-    public void launchCamera() {
-        // create Intent to take a picture and return control to the calling application
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Create a File reference for future access
-        photoFile = getPhotoFileUri(photoFileName);
 
-        // wrap File object into a content provider
-        // required for API >= 24
-        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-        //    Uri fileProvider = FileProvider.getUriForFile(CameraControlActivity.this, "in.nic.assam.libraries.capturegeotaggedimage.fileprovider", photoFile);
-        Uri fileProvider = FileProvider.getUriForFile(Objects.requireNonNull(getApplicationContext()),
-                BuildConfig.APPLICATION_ID + ".provider", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            // Start the image capture intent to take photo
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
-    }
-
-    // Returns the File for a photo stored on disk given the fileName
-    public File getPhotoFileUri(String fileName) {
-        // Get safe storage directory for photos
-        // Use `getExternalFilesDir` on Context to access package-specific directories.
-        // This way, we don't need to request external read/write runtime permissions.
-        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
-
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
-            Log.d(APP_TAG, "failed to create directory");
-        }
-
-        // Return the file target for the photo based on filename
-        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
-
-        return file;
-    }
 }
--- app/src/main/java/in/nic/assam/libraries/capturegeotaggedimage/MainActivity.java
+++ app/src/main/java/in/nic/assam/libraries/capturegeotaggedimage/MainActivity.java
@@ -1,32 +1,25 @@
 package in.nic.assam.libraries.capturegeotaggedimage;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
-import in.nic.assam.libraries.capturegeotaggedimage.image.GeoTaggedImage;
-import in.nic.assam.libraries.capturegeotaggedimage.image.ImageMetaData;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int CAMERA_CONTROL_ACTIVITY_REQUEST_CODE = 3434;
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private ImageView previewImageView;
     private Button captureButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        previewImageView = findViewById(R.id.preview_iv);
         captureButton = findViewById(R.id.capture_btn);
         captureButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,19 +38,6 @@ public class MainActivity extends AppCompatActivity {
             case (CAMERA_CONTROL_ACTIVITY_REQUEST_CODE) : {
                 if (resultCode == Activity.RESULT_OK) {
 
-                    ImageMetaData imageMetaData;
-                    imageMetaData = data.getParcelableExtra("captured_image");
-
-                    GeoTaggedImage geoTaggedImage = new GeoTaggedImage.Builder()
-                            .withContext(this)
-                            .withImageMetaData(imageMetaData)
-                            .withTextColor(Color.YELLOW)
-                            .withTextSize(55)
-                            .withGravity(0)
-                            .build();
-
-                    Bitmap processedBitmap = geoTaggedImage.processGeoTaggedImage();
-                    previewImageView.setImageBitmap(processedBitmap);
                 }
                 break;
             }
